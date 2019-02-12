56373661d9bca114bf5dd695743d0e839b6542f1
«Avsi222» <dorogin.arseniy@yandex.ru>
2019-02-12 12:17:33 +0300

    Добавлены арифметические операции

--- Task1/main.m
+++ Task1/main.m
@@ -11,6 +11,25 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
+        int first = 0;
+        int second = 0;
+        int third = 0;
+        
+        printf("Первое число: ");
+        scanf("%d", &first);
+        printf("Второе число: ");
+        scanf("%d", &second);
+        printf("Результат сложения: %d, Результат вычитания: %d, Результат деления:  %d, Результат умножения: %d \n", first + second,first - second,first / second,first * second);
+        
+        printf("Среднее арифметическое трех чисел \n");
+        printf("Введите три числа через пробел \n");
+        
+        scanf("%d %d %d", &first,&second,&third);
+        int AVG = (first+second+third)/3;
+        printf("Среднее арифметическое = %d \n",AVG);
+        
+        
+        
     }
     return 0;
 }
