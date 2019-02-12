beedfb5f748bd2ede76041559674e67b23f8818a
«Avsi222» <dorogin.arseniy@yandex.ru>
2019-02-12 11:38:02 +0300

    Initial Commit

---
+++ Task1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Task1
+//
+//  Created by Арсений Дорогин on 12/02/2019.
+//  Copyright © 2019 Арсений Дорогин. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
